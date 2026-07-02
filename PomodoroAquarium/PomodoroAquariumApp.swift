9e84c14b17db9652da7b35de3f6f705fd019fb10
gen <genkiboo0511@gmail.com>
2026-07-02 09:12:52 +0900

    Initial Commit

---
+++ PomodoroAquarium/PomodoroAquariumApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroAquariumApp.swift
+//  PomodoroAquarium
+//
+//  Created by 阿部弦生 on 2026/07/02.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroAquariumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PomodoroAquariumTests/PomodoroAquariumTests.swift
@@ -0,0 +1,19 @@
+//
+//  PomodoroAquariumTests.swift
+//  PomodoroAquariumTests
+//
+//  Created by 阿部弦生 on 2026/07/02.
+//
+
+import Testing
+@testable import PomodoroAquarium
+
+struct PomodoroAquariumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
