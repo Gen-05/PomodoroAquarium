5941c156e7cd92b9db546de70ca72ed0e548ccce
gen <genkiboo0511@gmail.com>
2026-07-06 09:22:31 +0900

    タイトルをポモドーロ水族館に変更

--- PomodoroAquarium/ContentView.swift
+++ PomodoroAquarium/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("ポモドーロ水族館")
         }
         .padding()
     }
