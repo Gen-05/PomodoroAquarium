6a9a4598deb54abdf1438f6309ab18cc7cd2fd21
gen <genkiboo0511@gmail.com>
2026-07-13 20:43:25 +0900

    プロジェクト構成を整理

---
+++ PomodoroAquarium/Views/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  PomodoroAquarium
+//
+//  Created by 阿部弦生 on 2026/07/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ PomodoroAquarium/Views/HomeView.swift
@@ -0,0 +1,48 @@
+//
+//  HomeView.swift
+//  PomodoroAquarium
+//
+//  Created by 阿部弦生 on 2026/07/11.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack(spacing: 30) {
+            
+            Text("🐠ポモドーロ水族館")
+                .font(.largeTitle)
+                .bold()
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.blue.opacity(0.2))
+                .frame(height: 250)
+                .overlay {
+                    Text("水槽エリア")
+                        .font(.title2)
+                }
+            
+            Text("25:00")
+                .font(.system(size: 50,weight: .bold))
+            
+            Button("勉強開始") {
+                
+            }
+            .buttonStyle(.borderedProminent)
+            
+            VStack {
+                Text("今日の勉強時間")
+                Text("0時間0分")
+                    .font(.title2)
+            }
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
