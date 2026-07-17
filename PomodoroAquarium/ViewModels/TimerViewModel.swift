d5134a072707aa6239b7022a0ea4e7ac545fdd33
gen <genkiboo0511@gmail.com>
2026-07-17 10:05:54 +0900

    タイマー状態をTimerViewModelへ移動

---
+++ PomodoroAquarium/ViewModels/TimerViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  TimerViewModel.swift
+//  PomodoroAquarium
+//
+//  Created by 阿部弦生 on 2026/07/17.
+//
+
+import SwiftUI
+
+struct TimerViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerViewModel()
+}
