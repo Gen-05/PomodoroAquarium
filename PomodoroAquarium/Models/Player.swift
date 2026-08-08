f9759ecd41878579ac824963ee8f7a452de2cdbc
gen <genkiboo0511@gmail.com>
2026-08-08 10:05:51 +0900

    PlayerとFishをSwiftDataに対応

---
+++ PomodoroAquarium/Models/Player.swift
@@ -0,0 +1,18 @@
+//
+//  Player.swift
+//  PomodoroAquarium
+//
+//  Created by 阿部弦生 on 2026/08/01.
+//
+
+import SwiftUI
+
+struct Player: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Player()
+}
---
+++ PomodoroAquarium/Models/PlayerData.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerData.swift
+//  PomodoroAquarium
+//
+//  Created by 阿部弦生 on 2026/08/01.
+//
+
+import SwiftUI
+
+struct PlayerData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerData()
+}
